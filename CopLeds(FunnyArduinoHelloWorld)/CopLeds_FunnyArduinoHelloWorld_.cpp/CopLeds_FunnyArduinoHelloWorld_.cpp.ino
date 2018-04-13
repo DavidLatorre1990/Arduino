62fe048f43aee45a2a365a4aff074a61a22f17ab
David Latorre <dvdn10071856@gmail.com>
2018-04-13 00:49:06 -0500

    added folder with cop leds project, including fritzing diagram, png image diagram, resources list and .cpp source code

---
+++ CopLeds(FunnyArduinoHelloWorld)/CopLeds_FunnyArduinoHelloWorld_.cpp/CopLeds_FunnyArduinoHelloWorld_.cpp.ino
@@ -0,0 +1,19 @@
+int azul = 13;
+int rojo = 7;
+int milisegundos = 300;
+
+void setup()
+{
+  pinMode(azul, OUTPUT);
+  pinMode(rojo, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(azul, HIGH);
+  digitalWrite(rojo, LOW);
+  delay(milisegundos);
+  digitalWrite(azul, LOW);
+  digitalWrite(rojo, HIGH);
+  delay(milisegundos * 2);
+}
