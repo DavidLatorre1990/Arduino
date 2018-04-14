372805214480a6e5471c02db3a912d92dadeb70b
David Latorre <dvdn10071856@gmail.com>
2018-04-14 15:01:14 -0500

    added Variable_Blink.ino folder with source code

---
+++ Variable_Blink.ino/Variable_Blink.ino/Variable_Blink.ino.ino
@@ -0,0 +1,23 @@
+int ledRojo = 3;
+int potPin = A0;
+int potValue = 0;
+
+void setup() 
+{
+
+  pinMode(ledRojo, OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() 
+{
+
+  potValue = analogRead(potPin);
+  Serial.println(potValue);
+  digitalWrite(ledRojo, HIGH);
+  delay (potValue);
+  digitalWrite(ledRojo, LOW);
+  delay (potValue);
+  
+}
